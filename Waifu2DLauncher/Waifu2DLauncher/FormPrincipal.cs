f8b8c9d0a4c5a7c6b8538e1fc1997f5440c64380
NioZero <ghidalgo@frostdisk.com>
2023-03-09 00:13:31 -0300

    Upload basic code

---
+++ Waifu2DLauncher/Waifu2DLauncher/FormPrincipal.cs
@@ -0,0 +1,10 @@
+namespace Waifu2DLauncher
+{
+    public partial class FormPrincipal : Form
+    {
+        public FormPrincipal()
+        {
+            InitializeComponent();
+        }
+    }
+}
